e6bb196e82e1f4e0892f33b6db5f6e63f91937a8
khaled <kaled21@std.mans.edu.eg>
2024-04-16 13:12:50 +0200

    make database UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'khaled',
+            'email'=>'khaled@gmail.com',
+            'password'=>Hash::make('password'),
+            'phone_number'=>'0106836951'
+        ]);
+    }
+}
